41bad49361eb818612a34fe048b4e5cc07a8501f
Anuj <invincibleanuj1718@gmail.com>
2024-07-19 17:54:57 +0530

    Initial commit

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,15 +1,48 @@
 import React from "react";
 
 const Navbar = () => {
+  const NavLinks = [
+    {
+      id: "1",
+      name: "HOME",
+      link: "/#",
+    },
+    {
+      id: "2",
+      name: "CARS",
+      link: "/#cars",
+    },
+    {
+      id: "3",
+      name: "ABOUT",
+      link: "/#about",
+    },
+    {
+      id: "4",
+      name: "BOOKING",
+      link: "/#booking",
+    },
+  ];
   return (
-    <nav>
+    <nav className="shadow-md">
       <div className="container">
-        <div className="flex">
+        <div className="flex justify-between items-center">
           <div>
-            <h1>Navbar</h1>
+            <h1 className="text-3xl font-bold font-serif">Car-Rental</h1>
           </div>
-          <div>
-            <ul></ul>
+          <div className="hidden md:block">
+            <ul className="flex items-center gap-8">
+              {NavLinks.map((data) => (
+                <li key={data.id} className="py-4">
+                  <a
+                    className="inline-block py-2 hover:border-b-2 hover:text-primary hover:border-primary transition-colors duration-500 text-lg font-medium"
+                    href={data.link}
+                  >
+                    {data.name}
+                  </a>
+                </li>
+              ))}
+            </ul>
           </div>
         </div>
       </div>
