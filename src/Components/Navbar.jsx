2f1ad6caaf87cb0c223dd3ed5f3b407d74cfd096
Anuj <invincibleanuj1718@gmail.com>
2024-07-19 17:25:12 +0530

    Navbar

---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <nav>
+      <div className="container">
+        <div className="flex">
+          <div>
+            <h1>Navbar</h1>
+          </div>
+          <div>
+            <ul></ul>
+          </div>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
